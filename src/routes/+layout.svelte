461a5ce33ea00c6a184ad264f2b29ff06c5d7032
Amir Karbasi <amir@karbasi.dev>
2024-02-13 19:37:45 -0500

    Replace highlightjs with prism

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,7 +5,6 @@
 	import '@skeletonlabs/skeleton/styles/skeleton.css';
 	// Most of your app wide CSS should be put in this file
 	import '../app.postcss';
-	import 'highlight.js/styles/github-dark.css';
 
 	import { AppBar, AppShell, LightSwitch } from '@skeletonlabs/skeleton';
 	import { page } from '$app/stores';
