e5d31b68219dd46b0f7e993a0ee0cb4b76827231
Amir Karbasi <amir@karbasi.dev>
2025-04-10 22:23:30 -0400

    Post card tweaks

--- ui/src/lib/components/post/PostCard.svelte
+++ ui/src/lib/components/post/PostCard.svelte
@@ -5,6 +5,7 @@
 		CardTitle,
 		CardDescription,
 		CardContent,
+		CardFooter,
 	} from '$lib/components/ui/card';
 	import { Badge } from '$lib/components/ui/badge';
 	import { CalendarIcon } from 'lucide-svelte';
@@ -27,7 +28,7 @@
 				<span>{formatDate(post.posted_at ?? post.created_at)}</span>
 				<div class="flex flex-wrap gap-2">
 					{#each post.tags as tag}
-						<Badge variant="secondary">{tag.name}</Badge>
+						<a href={`/tags/${tag.name}`}><Badge variant="outline">{tag.name}</Badge></a>
 					{/each}
 				</div>
 			</CardDescription>
@@ -35,5 +36,9 @@
 		<CardContent>
 			<p class="mb-4">{post.slug}</p>
 		</CardContent>
+		<CardFooter
+			><a class="hover:text-primary text-sm font-bold" href={`/blog/${post.slug}`}>Read more...</a
+			></CardFooter
+		>
 	</Card>
 </div>
